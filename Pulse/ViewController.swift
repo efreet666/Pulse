cb815df326a736a06d11573c7a2577adf62e6aae
Vlad <vladislav.bok@yandex.ru>
2022-07-24 16:48:11 +0300

    Initial Commit

---
+++ Pulse/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pulse
+//
+//  Created by Влад Бокин on 24.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
