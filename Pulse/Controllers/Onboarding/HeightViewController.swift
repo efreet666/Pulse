00a962a6fb32f65b3b8ce58127ad46f3d0ae64e8
Vlad <vladislav.bok@yandex.ru>
2022-07-25 18:18:09 +0300

    add segment, but bug

--- Pulse/Controllers/Onboarding/HeightViewController.swift
+++ Pulse/Controllers/Onboarding/HeightViewController.swift
@@ -7,17 +7,21 @@
 
 import UIKit
 
-
+protocol backButtonDelegate {
+    func popVC()
+}
 class HeightViewController: UIViewController {
 
+    private let backButton = BackButton()
     private let pageNumberTitle = PageNumberTitle()
     private let pageTextLabel = PageTextLabel()
+    private var segmentControl = UISegmentedControl()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.setHidesBackButton(true, animated: true)
-        
+        self.navigationController?.isNavigationBarHidden = true
         setupViews()
         setContraint()
     }
@@ -28,6 +32,14 @@ class HeightViewController: UIViewController {
         
         pageTextLabel.text = "What's your height?"
         view.addSubview(pageTextLabel)
+        
+        backButton.delegate = self
+        view.addSubview(backButton)
+        
+        let items = ["см", "ft"]
+        self.segmentControl = UISegmentedControl(items: items)
+        self.segmentControl.layer.cornerRadius = 14
+        view.addSubview(segmentControl)
     }
 
   
@@ -51,5 +63,24 @@ extension HeightViewController {
             make.leading.trailing.equalToSuperview().inset(74)
         }
         
+        backButton.snp.makeConstraints { make in
+            make.width.equalTo(15)
+            make.height.equalTo(15)
+            make.top.equalToSuperview().inset(60)
+            make.leading.equalToSuperview().inset(20)
+        }
+        
+        segmentControl.snp.makeConstraints { make in
+            make.top.equalToSuperview().inset(184)
+            make.leading.equalToSuperview().inset(116)
+            make.width.equalTo(146)
+            make.height.equalTo(48)
+        }
+    }
+}
+
+extension HeightViewController: backButtonDelegate {
+    func popVC() {
+        self.navigationController?.popToRootViewController(animated: true)
     }
 }
--- Pulse/Views/HeightVC/SegmentControl.swift
+++ Pulse/Views/HeightVC/SegmentControl.swift
@@ -1,34 +1,35 @@
 //
-//  BackButton.swift
+//  SegmentControl.swift
 //  Pulse
 //
-//  Created by Влад Бокин on 24.07.2022.
+//  Created by Влад Бокин on 25.07.2022.
 //
 
 import Foundation
 import UIKit
 
-class BackButton: UIButton {
-    
-    var delegate: backButtonDelegate?
+class SegmentControl: UISegmentedControl {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        configure()
+        configure(items2: [])
+        
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    func configure() {
-        setImage(UIImage(named: "Union"), for: .normal)
+    
+    func configure(items2: [String]) {
+        let items = ["Purple", "Green", "Blue"]
+        UISegmentedControl(items: items)
+        selectedSegmentIndex = 0
         addTarget(self, action: #selector(buttonTapped(button:)), for: .touchUpInside)
     }
     
     @objc func buttonTapped(button: UIButton){
-        print("tap1")
-        self.delegate?.popVC()
+
     }
 }
